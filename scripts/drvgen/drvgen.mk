e0d7da2393547d3c5cf27faa0bf59230b8fb63bf
Ertu <er2@dismail.de>
2021-04-20 10:07:06 +0300

    Some modifications

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -1,40 +1,36 @@
-ifdef MTK_PLATFORM
+# Copyright (C) 2016 MediaTek Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See http://www.gnu.org/licenses/gpl-2.0.html for more details.
 
-PRIVATE_CUSTOM_KERNEL_DCT := $(if $(CUSTOM_KERNEL_DCT),$(CUSTOM_KERNEL_DCT),dct)
-ifneq ($(wildcard $(srctree)/arch/arm/mach-$(MTK_PLATFORM)/$(MTK_PROJECT)/dct/$(PRIVATE_CUSTOM_KERNEL_DCT)/codegen.dws),)
-  DRVGEN_PATH := arch/arm/mach-$(MTK_PLATFORM)/$(MTK_PROJECT)/dct/$(PRIVATE_CUSTOM_KERNEL_DCT)
-else
-  DRVGEN_PATH := drivers/misc/mediatek/mach/$(MTK_PLATFORM)/$(MTK_PROJECT)/dct/$(PRIVATE_CUSTOM_KERNEL_DCT)
-endif
+ifdef MTK_PLATFORM
+DRVGEN_PATH := drivers/misc/mediatek/mach/$(MTK_PLATFORM)/$(MTK_PROJECT)/dct/dct
+
 ifndef DRVGEN_OUT
-#DRVGEN_OUT := $(objtree)/$(DRVGEN_PATH)
 DRVGEN_OUT := $(objtree)/arch/$(SRCARCH)/boot/dts
 endif
 export DRVGEN_OUT
 
 ALL_DRVGEN_FILE := cust.dtsi
 
-DWS_FILE := $(srctree)/$(DRVGEN_PATH)/codegen.dws
+DWS_FILE := $(srctree)/$(DRVGEN_PATH)/$(MTK_PROJECT).dws
 ifneq ($(wildcard $(DWS_FILE)),)
 DRVGEN_FILE_LIST := $(addprefix $(DRVGEN_OUT)/,$(ALL_DRVGEN_FILE))
 else
 DRVGEN_FILE_LIST :=
 endif
-DRVGEN_TOOL := $(srctree)/tools/dct/DrvGen
-DRVGEN_PREBUILT_PATH := $(srctree)/$(DRVGEN_PATH)
-DRVGEN_PREBUILT_CHECK := $(filter-out $(wildcard $(addprefix $(DRVGEN_PREBUILT_PATH)/,$(ALL_DRVGEN_FILE))),$(addprefix $(DRVGEN_PREBUILT_PATH)/,$(ALL_DRVGEN_FILE)))
+DRVGEN_TOOL := $(srctree)/tools/dct/DrvGen.py
 
 .PHONY: drvgen
 drvgen: $(DRVGEN_FILE_LIST)
-ifneq ($(DRVGEN_PREBUILT_CHECK),)
 $(DRVGEN_OUT)/cust.dtsi: $(DRVGEN_TOOL) $(DWS_FILE)
 	@mkdir -p $(dir $@)
-	$(DRVGEN_TOOL) $(DWS_FILE) $(dir $@) $(dir $@) cust_dtsi
-
-else
-$(DRVGEN_FILE_LIST): $(DRVGEN_OUT)/% : $(DRVGEN_PREBUILT_PATH)/%
-	@mkdir -p $(dir $@)
-	cp -f $< $@
-endif
+	python2 $^ $(dir $@) $(dir $@) cust_dtsi
 
 endif#MTK_PLATFORM
